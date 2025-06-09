affb450ed06126a93cb9817c31e6d92efc744f3b
ItsNotRicardo <142903523+IsNotRicardo@users.noreply.github.com>
2025-06-09 12:04:06 +0300

    Add batch mixer energy consumption script

---
+++ machines/batch_mixer/batch_mixer.py
@@ -0,0 +1,80 @@
+import math
+
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+
+HOURS_PER_DAY = 24
+
+WATTS_PER_KILOWATT = 1000
+
+# Number of data points in the CSV file
+# Unit: Minutes in a day
+DATA_POINTS = 1440
+
+# Variability of the data (to simulate realistic conditions)
+# Unit: Percentage as a decimal (0 to 1)
+VARIABILITY = 0.03
+
+# Time it takes to mix the raw materials in minutes
+# Unit: Minutes
+MIXING_TIME = 3
+
+# Time it takes to transfer raw materials from and to the mixer in minutes
+# Unit: Minutes
+TRANSFER_TIME = 2
+
+# Mixer power draw per ton of raw material mixed
+# Unit: Kilowatt per ton (kW/t)
+MIXER_POWER = 15
+
+# Amount of glass produced in one day
+# Unit: Tons (t)
+PRODUCTION_QUANTITY = 200
+
+# Raw material yield
+# Represents the amount of raw material that is converted into molten glass
+# The remaining percentage is lost in the melting process and escapes mostly as gas
+PRODUCTION_YIELD = 0.85
+
+# Calculate production duration
+cycle_time = MIXING_TIME + TRANSFER_TIME
+mixing_percentage = MIXING_TIME / cycle_time
+production_duration = mixing_percentage * DATA_POINTS
+
+# Calculate batch size
+material_amount = PRODUCTION_QUANTITY / PRODUCTION_YIELD
+batch_size = MIXING_TIME * material_amount / production_duration
+
+# Create a production segment
+production_segment = [batch_size] * MIXING_TIME
+production_segment.extend([0] * TRANSFER_TIME)
+
+# Create the production time series
+production_repetition = math.ceil(DATA_POINTS / cycle_time)
+production_series = np.tile(production_segment, production_repetition)
+production_series = production_series[:DATA_POINTS]
+
+# Calculate power consumption
+power_consumption = production_series * MIXER_POWER * WATTS_PER_KILOWATT
+
+# Introduce variability to power consumption
+np.random.seed(48)
+variable_power_consumption = np.random.normal(power_consumption, power_consumption * VARIABILITY)
+
+# Save to CSV
+df = pd.DataFrame({"ActivePower": variable_power_consumption})
+df.to_csv("batch_mixer.csv", index=False)
+
+# Visualize
+target_x = np.linspace(0, HOURS_PER_DAY, num=DATA_POINTS, endpoint=False)
+plt.figure(figsize=(12, 5))
+plt.plot(target_x, df['ActivePower'])
+plt.title("Batch Mixer Power Consumption")
+plt.xlabel("Hour of Day")
+plt.ylabel("Power Consumption (W)")
+plt.grid(True)
+plt.tight_layout()
+plt.show()
+
+print(f"Batch mixer CSV file generated successfully.")
